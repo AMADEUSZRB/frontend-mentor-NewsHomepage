bd9adf5904420f22d5b4d4546b1c6922b8aabaff
Robert Juszczyński <robertjuszczynski@o2.pl>
2023-02-26 16:18:46 +0100

    Project done.

--- menu.js
+++ menu.js
@@ -1,3 +1,12 @@
-function toggleMenu() {
-    document.querySelector(".navbar-list").style.right="unset";
-  }
+document.getElementById("menu-toggle").addEventListener("click", function toggleMenu() {
+  document.getElementById("overlay").style.display = "block";
+  document.querySelector(".navbar-list").style.right="0";
+});
+document.getElementById("menu-close").addEventListener("click", function CloseMenu() {
+  document.getElementById("overlay").style.display = "none";
+  document.querySelector(".navbar-list").style.right="-100vh";
+});
+document.getElementById("overlay").addEventListener("click", function CloseMenu() {
+  document.getElementById("overlay").style.display = "none";
+  document.querySelector(".navbar-list").style.right="-100vh";
+});
